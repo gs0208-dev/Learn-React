6982f96ac38648697dbd3b2909642975c9404d8a
Giridhar S <giridhar200402@gmail.com>
2026-01-13 19:18:12 +0530

    Refactor Main component to manage meme state and update topText dynamically

--- src/components/4-Side-Effects/Main.jsx
+++ src/components/4-Side-Effects/Main.jsx
@@ -1,28 +1,62 @@
+import { useState } from "react"
+
+/**
+ * Challenge: move the hardcoded meme info into React
+ * state. Use an object with `topText`, `bottomText`,
+ * and `imageUrl` properties, and set the initial values to
+ * the ones hardcoded below.
+*/
+
+/**
+ * Challenge: update the topText value in the meme state
+ * object every time the topText input box is changed
+ * 
+ * Note: don't worry about bottomText at this point.
+*/
+
+
 export default function Main() {
+
+  const [info, setInfo] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageURL: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  function handleChange(event){
+    const {value} = event.currentTarget;
+    setInfo(prev => ({
+      ...prev,
+      topText: value
+    }))
+    
+  }
+
   return (
     <main>
       <div className="form">
         <label>Top Text
-            <input
-              type="text"
-              placeholder="One does not simply"
-              name="topText"
-            />
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+          />
         </label>
 
         <label>Bottom Text
-            <input
-              type="text"
-              placeholder="Walk into Mordor"
-              name="bottomText"
-            />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+          />
         </label>
         <button>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={info.imageURL} />
+        <span className="top">{info.topText}</span>
+        <span className="bottom">{info.bottomText}</span>
       </div>
     </main>
   )
