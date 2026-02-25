9d82d643411876564b1492b476eab3116ce20e7f
Giridhar S <giridhar200402@gmail.com>
2026-02-25 19:23:31 +0530

    feat: - Used useEffect hook with it's clean up return function. - The meme generator mini project is completed.

--- src/App.jsx
+++ src/App.jsx
@@ -225,27 +225,68 @@ import Main from './components/4-Side-Effects/Main';
  * For now, just console.log("Effect function ran")
 */
 
+/**
+ * Challenge part 1:
+ * Fetch the data from this url: "https://swapi.dev/api/people/1"
+ * and save it in the starWarsData state. Make sure you don't
+ * get stuck in an infinite rendering loop!
+*/
+
+/**
+ * Challenge part 2:
+ * Combine the "count" state with the request URL
+ * so that pressing the "Get next character" button
+ * will get a new character from the Star Wars API.
+ * Remember to consider the dependencies array!
+*/
+
+
 function App() {
 
-  // return (
-  //   <>
-  //     <Header />
-  //     <Main />
-  //   </>
-  // )
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
 
-  const [starWarsData, setStarWarsData] = useState();
-  
-  fetch("https://swapi.info/api/people/1")
-  .then((res) => res.json())
-  .then(data => setStarWarsData(data));
+  /*
+  const [starWarsData, setStarWarsData] = useState({})
+  const [count, setCount] = useState(1)
+  console.log("Rendered");
+
+
+  useEffect(() => {
+    console.log("Re-rendered");
+
+    fetch(`https://swapi.info/api/people/${count}`)
+      .then((res) => res.json())
+      .then((json) => setStarWarsData(json))
+  }, [count])
 
   return (
     <div>
-      <pre>{JSON.stringify(starWarsData,null,2)}</pre>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   )
-  
+  */
+
+  /*
+  const [starWarsData, setStarWarsData] = useState();
+
+  fetch("https://swapi.info/api/people/1")
+    .then((res) => res.json())
+    .then(data => setStarWarsData(data));
+
+  return (
+    <div>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+    </div>
+  )
+  */
+
   /*
   const [count, setCount] = useState(0)
   console.log("Rendered");
@@ -305,7 +346,7 @@ function App() {
     />
     </div>
   )
-  */  
+  */
   /*
   const ninjaTurtles = [ <h2>Donatello</h2> , <h2>Michaelangelo</h2> , <h2>Rafael</h2>, <h2> Leonardo</h2>]
   return (
@@ -622,7 +663,7 @@ function App() {
       <Body userName={userName}/>
     </main>
   )
-  */  
+  */
   /*
   const [state, setState] = useState(pads);
 
--- src/components/4-Side-Effects/Main.jsx
+++ src/components/4-Side-Effects/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 /**
  * Challenge: move the hardcoded meme info into React
@@ -14,6 +14,30 @@ import { useState } from "react"
  * Note: don't worry about bottomText at this point.
 */
 
+ /**
+ * Challenge:
+ * Get an array of memes from the imgflip API as soon as
+ * this component renders for the first time.
+ * Check the imgflip documentation for the correct URL.
+ * Save the array of memes (not the whole response
+ * data) to state. (For this app, we'll randomly choose
+ * one of the memes from this array when the user clicks
+ * the "Get a new meme image" button, but we'll do that in
+ * a separate challenge.)
+ * 
+ * Hint: for now, don't try to use an async/await function.
+ * Instead, use `.then()` to resolve the promises
+ * from using `fetch`. We'll learn why after this challenge.
+*/
+
+/**
+ * Challenge: Get a random image from the array of
+ * allMemes when the user clicks the button. Once
+ * you've gotten a random image from the array, make
+ * sure to write the code that will display that
+ * random meme image to the page.
+*/
+
 
 export default function Main() {
 
@@ -23,6 +47,14 @@ export default function Main() {
     imageURL: "http://i.imgflip.com/1bij.jpg"
   })
 
+  const [allMemes, setAllMemes] = useState([])
+
+  useEffect(() => {
+    fetch(" https://api.imgflip.com/get_memes")
+    .then(res => res.json())
+    .then(data => setAllMemes(data.data.memes ));
+  },[])
+
   function handleChange(event){
     const {value,name} = event.currentTarget;
     setInfo(prev => ({
@@ -30,6 +62,19 @@ export default function Main() {
       [name]: value
     }))
     
+  }
+
+  function randomImageSelection(){
+    const random = Math.floor(Math.random()*40);
+
+  }
+
+  function handleClick(){
+    const random = Math.floor(Math.random()*40);
+    setInfo(prev => ({
+      ...prev,
+      imageURL: allMemes[random].url
+    }))
   }
 
   return (
@@ -52,7 +97,7 @@ export default function Main() {
             onChange={handleChange}
           />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={handleClick}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src={info.imageURL} />
